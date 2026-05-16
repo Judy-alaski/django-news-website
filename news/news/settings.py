198fbd0300b240af3c1fdb43ced3696284534ce0
Jude Urban <urban.11@yahoo.com>
2026-05-16 21:22:28 +0100

    Use env secret key

--- news/news/settings.py
+++ news/news/settings.py
@@ -35,7 +35,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l#jgnal--4b=ema$%73!p314)w3satwj88&3syh!a7^(4dc&ew'
+#SECRET_KEY ='django-insecure-l#jgnal--4b=ema$%73!p314)w3satwj88&3syh!a7^(4dc&ew'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
