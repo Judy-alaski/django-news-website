ee25cc1735390855c24e54ca0a06774034b3856b
Jude Urban <urban.11@yahoo.com>
2026-05-16 21:54:59 +0100

    Production security and SEO improvements

--- news/news/urls.py
+++ news/news/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
+from newsApp.sitemaps import ArticleSitemap
 from django.contrib.sitemaps import GenericSitemap
 from newsApp.models import Article
 
@@ -27,11 +28,16 @@ info_dict = {
     'date_field': 'published_date',  # make sure this field exists
 }
 
+sitemaps = {
+    'articles': ArticleSitemap,
+}
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('newsApp.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': {'articles': GenericSitemap(info_dict, priority=0.6)}}, name='django.contrib.sitemaps.views.sitemap'),
+    path('sitemap.xml',sitemap,{'sitemaps': sitemaps},name='django.contrib.sitemaps.views.sitemap'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
