3381dd73fddf746ef6af07247e473fd15ab7aa3e
Jude Urban <urban.11@yahoo.com>
2026-05-23 12:12:21 +0100

    Fixed robots and sitemap routes

--- news/news/urls.py
+++ news/news/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
 from django.views.generic import TemplateView
 from newsApp.sitemaps import ArticleSitemap
-from django.contrib.sitemaps import GenericSitemap
 from newsApp.models import Article
 
 info_dict = {
@@ -35,10 +34,34 @@ sitemaps = {
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('', include('newsApp.urls')),
+
     path('ckeditor/', include('ckeditor_uploader.urls')),
-    path('sitemap.xml',sitemap,{'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
-    path("robots.txt",TemplateView.as_view(template_name="robots.txt",content_type="text/plain")),
+
+    path(
+        'robots.txt',
+        TemplateView.as_view(
+            template_name='robots.txt',
+            content_type='text/plain'
+        ),
+    ),
+
+    path(
+        'sitemap.xml',
+        sitemap,
+        {'sitemaps': sitemaps},
+        name='django.contrib.sitemaps.views.sitemap'
+    ),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+#urlpatterns = [
+    #path('admin/', admin.site.urls),
+    #path('', include('newsApp.urls')),
+    #path('ckeditor/', include('ckeditor_uploader.urls')),
+    #path('sitemap.xml',sitemap,{'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    #path("robots.txt"#,TemplateView.as_view(template_name="robots.txt",content_type="text/plain")),
+#]
+
