b7a5ecfbacc5b2df1a0809e40af87c666a6605fc
Jude Urban <urban.11@yahoo.com>
2026-05-23 21:22:54 +0100

    Clean robots formatting

--- news/newsApp/views.py
+++ news/newsApp/views.py
@@ -269,7 +269,6 @@ def robots_txt(request):
     data = """
     User-agent: *
     Allow: /
-
     Sitemap: https://urbannews-xz7c.onrender.com/sitemap.xml
     """
     return HttpResponse(data, content_type="text/plain")
