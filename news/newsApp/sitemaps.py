98ca3702f7de3fc8a1d6f7f59bfad5950105fcac
Jude Urban <urban.11@yahoo.com>
2026-05-23 12:43:37 +0100

    Fixed sitemap absolute URLs

--- news/newsApp/sitemaps.py
+++ news/newsApp/sitemaps.py
@@ -2,11 +2,11 @@ from django.contrib.sitemaps import Sitemap
 from .models import Article
 
 class ArticleSitemap(Sitemap):
-    changefreq = "daily"
+    changefreq = "hourly"
     priority = 0.9
 
     def items(self):
-        return Article.objects.all()
+        return Article.objects.all().order_by('-published_date')
 
     def lastmod(self, obj):
         return obj.published_date
