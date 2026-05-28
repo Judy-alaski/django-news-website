37c94af5c3a758652891362e8fa9831ed7b6b79b
Jude Urban <urban.11@yahoo.com>
2026-05-28 19:06:43 +0100

    Url Updated

--- news/newsApp/urls.py
+++ news/newsApp/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     # Article Views
     #path('article/<int:id>/', views.article_detail, name='article_detail'),
     path('article/<slug:slug>/', views.article_detail, name='article_detail'),
-    path('article/<int:id>/', views.redirect_old_article),
+    path('article/<slug:slug>/', views.redirect_old_article),
     path('upload/', views.article_upload, name='article_upload'),
     path('add_article/', views.add_article, name='add_article'),
 
